06051de556a5783c1055c5caaa6ef5968198d674
siny-over-cosy <elitan.uk@gmail.com>
2025-07-21 00:07:42 +0100

    changed default font

--- src/render.cpp
+++ src/render.cpp
@@ -7,12 +7,12 @@ void render_img(std::string date) {
   Magick::Image newImg("../img/template.png");
 
   // font shenanigans
-  newImg.strokeWidth(1);
-  newImg.font("Adwaita-Sans-Extralight"); // change to a font that
-                                          // your OS has installed if
-                                          // program throws errors or doesn't
-                                          // work as expected
-                                          // 99% chance this will be the cause of it
+  newImg.strokeWidth(2);
+  newImg.font("../fonts/work-sans-extra-light.ttf"); // change to a font that
+                                              // your OS has installed if
+                                              // program throws errors or doesn't
+                                              // work as expected
+                                              // 99% chance this will be the cause of it
   newImg.fontPointsize(50);
 
   // draw text onto template
